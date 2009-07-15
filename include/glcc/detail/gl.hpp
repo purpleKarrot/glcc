745ecaf4b53c3ce947e615869f9b9b1f1be2ad8d
Daniel Pfeifer <daniel@pfeifer-mail.de>
2009-07-15 08:18:53 +0000

    files from opengl registry

--- include/glcc/detail/gl.hpp
+++ include/glcc/detail/gl.hpp
@@ -1,56 +1,43 @@
-/*
- * gl.hpp
- *
- *  Created on: 08.09.2008
- *      Author: daniel
- */
+/**************************************************************
+ * Copyright (c) 2008-2009 Daniel Pfeifer                     *
+ *                                                            *
+ * Distributed under the Boost Software License, Version 1.0. *
+ **************************************************************/
 
 #ifndef GLCC_DETAIL_GL_HPP
 #define GLCC_DETAIL_GL_HPP
 
-#define GL3_PROTOTYPES 1
-#include <GL3/gl3.h>
-#define __gl_h_
-#define __glext_h_
+#ifdef WIN32
 
-//#ifdef BOOST_WINDOWS
-//
-//#define WIN32_LEAN_AND_MEAN
-//#include <windows.h>
-//#include <GL/gl.h>
-//#include <boost/gl/detail/ext/glext.h>
-//#include <boost/gl/detail/ext/wglext.h>
-//
-//#define BOOST_GL_WGL 1
-//
-//#ifdef _MSC_VER
-//#pragma comment(lib, "OpenGL32.lib")
-//#endif
-//
-//#elif defined(__APPLE__)
-//
-//#ifndef GLCC_NO_EXTPOINTER
-//#define GL_GLEXT_LEGACY 1
-//#endif
-//
-//#include <OpenGL/gl.h>
-//#include <boost/gl/detail/ext/glext.h>
-//
-//#else
-//
-//#define GL_GLEXT_LEGACY 1
-//#include <GL/gl.h>
-//
-//#define GLX_GLXEXT_LEGACY 1
-//#include <GL/glx.h>
-//
-//#define GL_GLEXT_PROTOTYPES 1
-//#include <boost/gl/detail/ext/glext.h>
-//
-//#define GLX_GLXEXT_PROTOTYPES 1
-//#include <boost/gl/detail/ext/glxext.h>
-//#define BOOST_GL_GLX 1
-//
-//#endif
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <GL/gl.h>
+#include <glcc/detail/ext/glext.h>
+#include <glcc/detail/ext/wglext.h>
+
+#ifdef _MSC_VER
+#pragma comment(lib, "OpenGL32.lib")
+#endif
+
+#elif defined(__APPLE__)
+
+#include <OpenGL/gl.h>
+#include <glcc/detail/ext/glext.h>
+
+#else
+
+#define GL_GLEXT_LEGACY 1
+#include <GL/gl.h>
+
+#define GLX_GLXEXT_LEGACY 1
+#include <GL/glx.h>
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <glcc/detail/ext/glext.h>
+
+#define GLX_GLXEXT_PROTOTYPES 1
+#include <glcc/detail/ext/glxext.h>
+
+#endif
 
 #endif /* GLCC_DETAIL_GL_HPP */
