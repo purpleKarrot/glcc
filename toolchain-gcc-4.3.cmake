62bb433c5aae8816d420a00103a35cac5cb10043
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-04-11 10:17:43 +0200

    zipped Boost.LA

--- toolchain-gcc-4.3.cmake
+++ toolchain-gcc-4.3.cmake
@@ -1,5 +1,5 @@
 
-# gcc 4.4.1 has problems with GIL, so in the meantime we use gcc 4.3
+# gcc 4.4 has some bugs in ubuntu 10.04
 # cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-gcc-4.3.cmake ..
 
 set(CMAKE_C_COMPILER   gcc-4.3)
